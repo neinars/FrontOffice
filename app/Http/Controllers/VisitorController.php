5b093950ac2e54e0a4ff0a838838cb8297d2019b
Neina Rahma Sari <1klan4dwords.nr@gmail.com>
2022-10-12 14:38:44 +0700

    12/10/22 make mcr

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visitor;
+use Illuminate\Http\Request;
+
+class VisitorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('visitor');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Visitor::create([
+            "name" => $request->name,
+            "address" => $request->address,
+            "instance" => $request->instance,
+            "phone_number" => $request->phone_number,
+            "meet_with" => $request->meet_with,
+            "utilities" => $request->utilities,
+        ]);
+
+        return redirect()->back()->with('status', 'Success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Visitor $visitor)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/visitor', [App\Http\Controllers\VisitorController::class, 'index'])->name('buku.tamu');
+
