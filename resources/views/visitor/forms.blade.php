a20a3087bfb4877e004ed0cd9cd15b1464d1f0b8
Neina Rahma Sari <1klan4dwords.nr@gmail.com>
2022-10-25 16:17:21 +0700

    25/10/22 teacher

--- resources/views/visitor/forms.blade.php
+++ resources/views/visitor/forms.blade.php
@@ -26,6 +26,7 @@
                 <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
+<<<<<<< HEAD
     </div>
 </div>
 @endforeach
@@ -53,6 +54,10 @@
                     </div>
                 </div>
         </div>
+=======
+</div>
+</div>
+>>>>>>> da2b44f (21/10/22 add-show visitor clear)
         @endforeach
 
 {{-- MODAL ADD --}}
